011bd57e1bde65d9180dc8ecf3599c4c451897ae
Austin <lyvuaustin@gmail.com>
2024-07-09 16:34:37 -0500

    Initial Setup Files

---
+++ src/test/java/TestRunner/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/alv/resources/features", glue = "StepDefinition", monochrome = true, plugin = {
+        "pretty", "html:target/Reports/report.html", "junit:target/Reports/report.xml" })
+public class RunCucumberTest {
+}
